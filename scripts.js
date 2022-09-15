d1dfa460cc0c55f6e16b413fc53796d6c1301293
enix <56111821+ppodkowka@users.noreply.github.com>
2022-09-15 18:34:54 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,31 +1,8 @@
-let btnForward = document.querySelector('#btnForward');
-
-btnForward.addEventListener('click',() => btnForward.style.backgroundColor='#337ab7')
-
-
-
-
-
-
-
-async function left() {
-    await fetch("left", { method: "POST" });
+function updateSliderPWM(element) {
+    var pwmSliderValue = document.getElementById("pwmSlider").value;
+    document.getElementById("textSliderValue").innerHTML = pwmSliderValue;
+    console.log(pwmSliderValue);
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.open("GET", "/slider?value="+pwmSliderValue, true);
+    httpRequest.send();
 }
-
-async function forward() {
-    document.getElementById("btn").style.color = "black";
-    await fetch("forward", { method: "POST" });
-}
-
-async function right() {
-    await fetch("right", { method: "POST" });
-}
-
-async function back() {
-    await fetch("backward", { method: "POST" });
-}
-
-async function stop() {
-    await fetch("stop", { method: "POST" });
-}
-
