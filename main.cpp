08f3650331dd73ddae5803cc7b929de234f49091
enix <56111821+ppodkowka@users.noreply.github.com>
2022-09-17 23:38:53 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,51 +4,43 @@
 #include "ESPAsyncWebServer.h"
 #include "SPIFFS.h"
 
-//wifi autka i haslo
 const char* ssid = "ESP_CAR";
 const char* password = "123456789";
 
-// Zdefiniowanie silniczkow A i B
+// Zdefiniowanie Motora A i B
 int motor1Pin1 = 27; 
 int motor1Pin2 = 26; 
 int enable1Pin = 14;
-
 int motor2Pin1 = 23; 
 int motor2Pin2 = 22; 
 int enable2Pin = 12;  
 
-String MotorState;
-
-String pwmSliderValue = "0";
-
-// Setting PWM properties
-const int freq = 30000;
-const int pwmChannel = 0;
-const int resolution = 8;
-//int car_speed = 255;
-
-//wartosci do zmiany predkosci autka
+String carSpeed = "0"; // duty cycle
 const char* PARAM_INPUT = "value";
 
+// Setting PWM properties
+const int freq = 500;
+const int pwmChannel = 0;
+const int resolution = 8;
 
-// Create AsyncWebServer object on port 80
-AsyncWebServer server(80);
-
-// Replaces placeholder with MOTOR state value
+//dzieki tej funkcji podmieniam SLIDERVALUE na faktyczna liczbe
 String processor(const String& var){
   Serial.println(var);
-  if (var == "SLIDERVALUE"){
-    return pwmSliderValue;
+  if (var == "CARSPEED"){
+    return carSpeed;
   }
   return String();
 }
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
 
 
 void setup() {
 
   Serial.begin(115200);
 
-  //ustawiam ze piny sa wyjsciowe do sterowania silnikami
+  //inicjalizacja motorów
   pinMode(motor1Pin1, OUTPUT);
   pinMode(motor1Pin2, OUTPUT);
   pinMode(enable1Pin, OUTPUT);
@@ -57,23 +49,21 @@ void setup() {
   pinMode(motor2Pin2, OUTPUT);
   pinMode(enable2Pin, OUTPUT);
 
-  // konfiguracja PWM
+  // configure LED PWM functionalitites
   ledcSetup(pwmChannel, freq, resolution);
 
   // attach the channel to the GPIOs to be controlled
   ledcAttachPin(enable1Pin, pwmChannel);
   ledcAttachPin(enable2Pin, pwmChannel);
 
-  //car_speed = sliderValue.toInt();
-  ledcWrite(pwmChannel, pwmSliderValue.toInt());
+  ledcWrite(pwmChannel, carSpeed.toInt());
 
-  //ustawiam kazdy pin na 0 bo domyslnie autko ma stac w miejscu
   digitalWrite(motor1Pin1, LOW); 
   digitalWrite(motor1Pin2, LOW);
   digitalWrite(motor2Pin1, LOW); 
   digitalWrite(motor2Pin2, LOW);
 
-  // Inicjalizacja biblioteki SPIFFS
+  // Initialize SPIFFS
   if(!SPIFFS.begin(true)){
     Serial.println("An Error has occurred while mounting SPIFFS");
     return;
@@ -82,7 +72,6 @@ void setup() {
   //robie z esp wlasna siec wifi
   Serial.print("Setting AP (Access Point)…\n");
   WiFi.softAP(ssid, password);
-
   IPAddress IP = WiFi.softAPIP();
   Serial.print("AP IP address: ");
   Serial.println(IP);
@@ -97,16 +86,38 @@ void setup() {
     request->send(SPIFFS, "/style.css", "text/css");
   });
 
+  //load scripts.js file
+  server.on("/scripts.js", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/scripts.js", "text/javascript");
+  });
+
 //-------------------------------------------------------------------------
+
+  server.on("/slider", HTTP_GET, [] (AsyncWebServerRequest *request) {
+  String inputMessage;
+  // GET input1 value on <ESP_IP>/slider?value=<inputMessage>
+  if (request->hasParam(PARAM_INPUT)) {
+    inputMessage = request->getParam(PARAM_INPUT)->value();
+    carSpeed = inputMessage;
+    ledcWrite(pwmChannel, carSpeed.toInt());
+  }
+  else {
+    inputMessage = "No message sent";
+  }
+  Serial.println(inputMessage);
+  //request->send(200, "text/plain", "OK");
+  request->send(SPIFFS, "/index.html");
+  });
+
     // STOP MOTOR
   server.on("/stop", HTTP_GET, [](AsyncWebServerRequest *request){
     digitalWrite(motor1Pin1, LOW); 
     digitalWrite(motor1Pin2, LOW);
     digitalWrite(motor2Pin1, LOW); 
     digitalWrite(motor2Pin2, LOW);
-    ledcWrite(pwmChannel, pwmSliderValue.toInt());
     //delay(2000);
-    request->send(SPIFFS, "/index.html", String(), false, processor);
+    ledcWrite(pwmChannel, carSpeed.toInt());    
+    request->send(SPIFFS, "/index.html");
   });
   
   // FORWARD
@@ -115,9 +126,9 @@ void setup() {
     digitalWrite(motor1Pin2, HIGH);
     digitalWrite(motor2Pin1, LOW); 
     digitalWrite(motor2Pin2, HIGH);
-    ledcWrite(pwmChannel, pwmSliderValue.toInt());
     //delay(2000);
-    request->send(SPIFFS, "/index.html", String(), false, processor);
+    ledcWrite(pwmChannel, carSpeed.toInt());
+    request->send(SPIFFS, "/index.html");
   });
 
   // BACKWARD
@@ -126,9 +137,9 @@ void setup() {
     digitalWrite(motor1Pin2, LOW);
     digitalWrite(motor2Pin1, HIGH); 
     digitalWrite(motor2Pin2, LOW);
-    ledcWrite(pwmChannel, pwmSliderValue.toInt());
     //delay(2000);
-    request->send(SPIFFS, "/index.html", String(), false, processor);
+    ledcWrite(pwmChannel, carSpeed.toInt());
+    request->send(SPIFFS, "/index.html");
   });
 
    // LEFT
@@ -137,10 +148,9 @@ void setup() {
     digitalWrite(motor1Pin2, LOW);
     digitalWrite(motor2Pin1, LOW); 
     digitalWrite(motor2Pin2, HIGH);
-    ledcWrite(pwmChannel, pwmSliderValue.toInt());
-
     //delay(2000);
-    request->send(SPIFFS, "/index.html", String(), false, processor);
+    ledcWrite(pwmChannel, carSpeed.toInt());
+    request->send(SPIFFS, "/index.html");
   });
 
    // RIGHT
@@ -149,29 +159,15 @@ void setup() {
     digitalWrite(motor1Pin2, HIGH);
     digitalWrite(motor2Pin1, LOW); 
     digitalWrite(motor2Pin2, LOW);
-    ledcWrite(pwmChannel, pwmSliderValue.toInt());
     //delay(2000);
-    request->send(SPIFFS, "/index.html", String(), false, processor);
-  });
-
-  server.on("/slider", HTTP_GET, [] (AsyncWebServerRequest *request) {
-    String inputMessage;
-    // GET input1 value on <ESP_IP>/slider?value=<inputMessage>
-    if (request->hasParam(PARAM_INPUT)) {
-      inputMessage = request->getParam(PARAM_INPUT)->value();
-      pwmSliderValue = inputMessage;
-      //car_speed = sliderValue.toInt();
-      ledcWrite(pwmChannel, pwmSliderValue.toInt());
-    }
-    else {
-      inputMessage = "No message sent";
-    }
-    Serial.println(inputMessage);
-    request->send(SPIFFS, "text/plain", "OK");
+    ledcWrite(pwmChannel, carSpeed.toInt());
+    request->send(SPIFFS, "/index.html");
   });
  
+
   // Start server
   server.begin();
+
 
 }
 
